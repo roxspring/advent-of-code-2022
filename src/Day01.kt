beec288a0dd75d64ef5540c017b67498600a52a3
Rob Oxspring <rob.oxspring@diffblue.com>
2022-12-01 21:37:16 +0000

    Day01 Done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input.chunkedByBlank()
+        .map { lines -> lines.sumOf { it.toInt() } }
+        .max()
+
+    fun part2(input: List<String>): Int = input.chunkedByBlank()
+        .map { lines -> lines.sumOf { it.toInt() } }
+        .sortedDescending()
+        .take(3)
+        .sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
+
+    check(part2(testInput) == 45000)
     println(part2(input))
 }
+
+/**
+ * Splits the list into chunks delimited by blank lines.
+ *
+ * @return a sequence of lists of non-blank lines.
+ */
+fun List<String>.chunkedByBlank(): Sequence<List<String>> = sequence {
+    var start = 0
+    forEachIndexed { index, line ->
+        if (line.isEmpty()) {
+            yield(subList(start, index))
+            start = index + 1
+        }
+    }
+    yield(subList(start, size))
+}
+
